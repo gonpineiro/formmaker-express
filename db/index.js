ac63e8be33b93710c55fa8745d2812977d3ac01f
Gonzalo <gon.pineiro@gmail.com>
2021-09-16 12:26:26 -0300

    :hammer: dot-env

--- db/index.js
+++ db/index.js
@@ -1,10 +1,8 @@
 const mongoose = require("mongoose");
-process.env.DB_HOST
-mongoose
-  .connect(process.env.DB_HOST, { useNewUrlParser: true })
-  .catch((e) => {
-    console.error("Connection error", e.message);
-  });
+
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }).catch((e) => {
+  console.error("Connection error", e.message);
+});
 
 const db = mongoose.connection;
 
